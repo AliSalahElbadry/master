ba0a299e449d3e65f023daf5a72bfafdee6c4fa0
ali <ali@ali.com>
2022-12-16 16:56:02 +0200

    add file HelloIti.java

---
+++ git_f1/HelloIti.java
@@ -0,0 +1,14 @@
+public class HelloIti{
+
+  public static void main(String []args){
+  
+	System.out.println("HelloIti !");
+  
+  }
+  public static void print(){
+  
+	System.out.println("HelloI Git");
+  
+  }
+
+}
